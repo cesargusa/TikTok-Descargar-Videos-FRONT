d72631b5f0a736330b5d16182809c52c6333144d
cesargusa <cesargusa22@gmail.com>
2023-03-14 20:25:09 +0100

    añadir objeto json mockeado con datos reales, llamada onload para mostrar log con los datos json

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,16 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
 */
 // This file is intentionally blank
+
 // Use this file to add JavaScript to your project
+
+
+window.onload = () => {
+    //Datos mockeados con prueba para que no gaste
+    //las 160 peticiones mensuales
+    fetch('./prueba.json')
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+}
